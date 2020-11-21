c2cbba77335b3f94296000df4dc333b19323a96f
OMouad <71100063+OMouad@users.noreply.github.com>
2020-11-21 02:28:17 +0100

    import pickle and os modules to create a save file

--- contactbook.py
+++ contactbook.py
@@ -1,3 +1,15 @@
+import pickle
+import os
+
+filesize = os.path.getsize("contactbook.pkl")
+contactbookread = open("contactbook.pkl", 'rb')
+
+if filesize == 0:
+    contactlist = {}
+else:
+    contactlist = pickle.load(contactbookread)
+
+
 def cleanword(n):
     n = n.lower()
     n = n.strip()
